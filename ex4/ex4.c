ce8ccd76a4cd5a140d83114826269643de4457f5
chobe <soy_chobe@yahoo.com>
2015-09-20 21:39:54 -0600

    add exercise 4

---
+++ ex4/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose. */
+
+int main(int argc, char* argv[])
+{
+  int age = 10;
+  int heigh;
+
+  printf("I am %d years old.\n");
+  printf("I am %d inches tall.\n", heigh);
+
+  return 0;
+}
